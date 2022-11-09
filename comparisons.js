8c697cd666535544aee55241a5f491eabb5ab701
Joshua Martin <ursa8minority@gmail.com>
2022-11-09 13:06:28 -0700

    Add Part 1 answers

--- comparisons.js
+++ comparisons.js
@@ -17,35 +17,35 @@ var stringTeachers = "4"
 var numberDogs = 0
 
 // EXAMPLE: log the result of the comparison: is numberTeachers greater than numberStudents?
-
+console.log("Is numberTeachers greater than numberStudents?", numberTeachers > numberStudents);
 // this should log: "Is numberTeachers greater than numberStudents?" false
 
 // YOU DO: log the result of the comparison: is numberTeachers less than numberStudents?
-
+console.log("Is numberTeachers less than numberStudents", numberTeachers < numberStudents);
 // this should log: true
 
 // YOU DO: log the result of the comparison: is numberTeachers strictly equal to stringTeachers?
-
+console.log("Is numberTeachers strictly equal to stringTeachers", numberTeachers === stringTeachers);
 // this should log: false
 
 // YOU DO: log the result of the comparison: is numberTeachers not equal to numberStudents?
-
+console.log("is numberTeachers not equal to numberStudents?", numberTeachers !== numberStudents);
 // this should log: true
 
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 20?
-
+console.log("is numberStudents greater than or equal to 20?", numberStudents >= 20)
 // this should log: true
 
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 21?
-
+console.log(" is numberStudents greater than or equal to 21?", numberStudents >= 21);
 // this should log: false
 
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 20?
-
+console.log("is numberStudents less than or equal to 20?", numberStudents <= 20)
 // this should log: true
 
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 21?
-
+console.log("is numberStudents less than or equal to 21?", numberStudents <= 21)
 // this should log: true
 
 
